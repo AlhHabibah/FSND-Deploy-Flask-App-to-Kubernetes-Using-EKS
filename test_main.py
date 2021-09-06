cd41053499cb40c0bde1031336ad193e9164fdc9
AlhHabibah <yohabibah-alharbi@hotmail.com>
2021-09-06 04:52:50 +0300

    habibahCommit

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ import pytest
 
 import main
 
-SECRET = 'TestSecret'
+SECRET = os.environ.get(Secret)    // "TestSecret"
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
-EMAIL = 'wolf@thedoor.com'
-PASSWORD = 'huff-puff'
+EMAIL = os.environ.get(Email)                            //'wolf@thedoor.com'
+PASSWORD = os.environ.get(Password) //'huff-puff'
 
 @pytest.fixture
 def client():
